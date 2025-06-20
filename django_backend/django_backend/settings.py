cf0375877a600a3fbb1696bb83788381d79fcc5d
Kishu-Harshit <harshitnayan12a12@gmail.com>
2025-06-20 20:47:22 +0530

    Added Django REST Framework, CORS headers, and Decouple

--- django_backend/django_backend/settings.py
+++ django_backend/django_backend/settings.py
@@ -37,9 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'core',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -68,6 +72,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'django_backend.wsgi.application'
 
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # React or your frontend port
+    "http://localhost:3001",  # Node backend
+]
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
