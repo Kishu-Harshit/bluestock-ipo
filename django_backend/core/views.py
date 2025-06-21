0a49a76ea4637a7c8c6d6aa92d7e06948d8d3f25
Kishu-Harshit <harshitnayan12a12@gmail.com>
2025-06-21 14:57:40 +0530

    All set

--- django_backend/core/views.py
+++ django_backend/core/views.py
@@ -10,10 +10,9 @@ from django.http import HttpResponse, JsonResponse
 from django.conf import settings
 
 def frontend_view(request):
-    index_path = os.path.join(settings.BASE_DIR, '..', 'frontend', 'index.html')
-    index_path = os.path.abspath(index_path)
-    with open(index_path, 'r', encoding='utf-8') as f:
-        return HttpResponse(f.read())
+    #index_path = os.path.join(settings.BASE_DIR, '..', 'home.html')
+    #index_path = os.path.abspath(index_path)
+    return render(request, 'home.html')
 
 def call_node_backend(request):
     data_to_send = {'name': 'Django', 'message': 'Hello from Django!'}
--- django_backend/django_backend/settings.py
+++ django_backend/django_backend/settings.py
@@ -124,10 +124,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'frontend/'
 
 STATICFILES_DIRS = [
     os.path.abspath(os.path.join(BASE_DIR, "..", "frontend")),
+    os.path.abspath(os.path.join(BASE_DIR, "..", "shared", "static")),
 ]
 
 # Default primary key field type
